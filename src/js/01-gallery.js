75903b849e967f62ca22857d7fc706f1c504309d
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-09-14 08:14:17 +0300

    made first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+console.log(SimpleLightbox);
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createGallaryCards(galleryItems);
+
+function createGallaryCards(cards) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+
+// console.log(galleryItems);
